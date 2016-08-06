a1c6ae36a7ec43819c67743daf49314d241b84cd
pauldstar <paulogbeiwi@gmail.com>
2016-08-06 13:08:43 +0100

    ignore functionality and 'terms and conditions' files

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+include 'stdlib.php';
+
+$site = new csite();
+
+// this is a function specific to this site!
+initialise_site($site);
+
+$page = new cpage("Welcome to my site!");
+$site->setPage($page);
+
+$content = <<<EOT Welcome to my personal web site! EOT;
+$page->setContent($content);
+
+$site->render();
