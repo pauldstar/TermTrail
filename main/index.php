b6c50ce9e7b7490448818446488ad04e4bce4279
pauldstar <paulogbeiwi@gmail.com>
2016-08-07 05:14:06 +0100

    introducing code igniter 3 framework

--- main/index.php
+++ main/index.php
@@ -11,7 +11,7 @@ $content = "Welcome to my personal web site!";
 $index_page->setContent($content);
 
 $site->setPage($index_page);
-$site->addHeader("layout/header.html");
-$site->addFooter("layout/footer.html");
+$site->addHeader("content/header.html");
+$site->addFooter("content/footer.html");
 
 $site->render();
