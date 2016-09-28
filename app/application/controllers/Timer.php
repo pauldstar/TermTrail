0db46ed6bc41450c2853b7c54e643781d5440b50
pauldstar <paulogbeiwi@gmail.com>
2016-09-29 00:34:37 +0100

    completed ER dia; added functionality to docs

--- app/application/controllers/Timer.php
+++ app/application/controllers/Timer.php
@@ -1,52 +1,50 @@
 <?php
-
 class Timer {
-
   var $classname = "Timer";
   var $start = 0;
   var $stop = 0;
   var $elapsed = 0;
-
-  # Constructor
+  
+  // Constructor
   public function __construct($start = true) {
-    if ( $start )
+    if ($start)
       $this->start();
   }
-
-  # Start counting time
+  
+  // Start counting time
   public function start() {
     $this->start = $this->_gettime();
   }
-
-  # Stop counting time
+  
+  // Stop counting time
   public function stop() {
     $this->stop = $this->_gettime();
     $this->elapsed = $this->_compute();
   }
-   
-  # Get Elapsed Time
+  
+  // Get Elapsed Time
   public function elapsed() {
-    if ( !$elapsed )
+    if (! $elapsed)
       $this->stop();
-
-      return $this->elapsed;
+    
+    return $this->elapsed;
   }
-   
-  # Get Elapsed Time
+  
+  // Get Elapsed Time
   public function reset() {
     $this->start = 0;
     $this->stop = 0;
     $this->elapsed = 0;
   }
-
-  # Get Current Time
+  
+  // Get Current Time
   private function _gettime() {
     $mtime = microtime();
-    $mtime = explode( " ", $mtime );
-    return $mtime[1] + $mtime[0];
+    $mtime = explode(" ", $mtime);
+    return $mtime [1] + $mtime [0];
   }
-   
-  # Compute elapsed time
+  
+  // Compute elapsed time
   private function _compute() {
     return $this->stop - $this->start;
   }
